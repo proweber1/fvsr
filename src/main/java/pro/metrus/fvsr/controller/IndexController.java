38f88f4f29c8c71d1abb30ced144938f1ddaff74
Vjachelsav Gusser <proweber1@yandex.ru>
2017-06-19 19:32:51 +0300

    simple template

---
+++ src/main/java/pro/metrus/fvsr/controller/IndexController.java
@@ -0,0 +1,13 @@
+package pro.metrus.fvsr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    private String index() {
+        return "index";
+    }
+}
