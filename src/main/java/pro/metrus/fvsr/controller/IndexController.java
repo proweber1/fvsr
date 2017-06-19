e4a9bf4e6a91d73b346316f7be49ebf9b206ee03
Vjachelsav Gusser <proweber1@yandex.ru>
2017-06-19 19:37:32 +0300

    add javadoc to index controller for class level and index action

--- src/main/java/pro/metrus/fvsr/controller/IndexController.java
+++ src/main/java/pro/metrus/fvsr/controller/IndexController.java
@@ -3,9 +3,18 @@ package pro.metrus.fvsr.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * This is home controller.
+ */
 @Controller
 public class IndexController {
 
+    /**
+     * This method is simple. Method just return home page
+     * template name.
+     *
+     * @return Home page view name
+     */
     @GetMapping("/")
     private String index() {
         return "index";
