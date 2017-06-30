91ea89ba7bf8f0c1f4b18d99a2d18836b673e964
Vjachelsav Gusser <proweber1@yandex.ru>
2017-06-30 16:36:25 +0300

    create race endpoint

--- src/main/java/pro/metrus/fvsr/controllers/RacesController.java
+++ src/main/java/pro/metrus/fvsr/controllers/RacesController.java
@@ -5,8 +5,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.metrus.fvsr.domains.Category;
+import pro.metrus.fvsr.domains.Race;
+import pro.metrus.fvsr.domains.RaceType;
+import pro.metrus.fvsr.repositories.CategoriesRepository;
+import pro.metrus.fvsr.repositories.RaceTypesRepository;
 import pro.metrus.fvsr.repositories.RacesRepository;
+
+import java.util.List;
 
 /**
  * Controller which work with races
@@ -21,14 +29,53 @@ public class RacesController {
     private final RacesRepository racesRepository;
 
     /**
+     * Repository which work with race types
+     */
+    private final RaceTypesRepository raceTypesRepository;
+
+    /**
+     * Repository which work with categories
+     */
+    private final CategoriesRepository categoriesRepository;
+
+    /**
      * Dependency injection constructor
      *
-     * @param racesRepository Repository which work with races, must be not
-     *                        null
+     * @param racesRepository      Repository which work with races, must be not
+     *                             null
+     * @param raceTypesRepository  Repository for work with race types, must be not null
+     * @param categoriesRepository Repository for work with categories, must be not null
      */
     @Autowired
-    public RacesController(final RacesRepository racesRepository) {
+    public RacesController(
+            final RacesRepository racesRepository,
+            final RaceTypesRepository raceTypesRepository,
+            final CategoriesRepository categoriesRepository
+    ) {
         this.racesRepository = racesRepository;
+        this.raceTypesRepository = raceTypesRepository;
+        this.categoriesRepository = categoriesRepository;
+    }
+
+    /**
+     * This method load all race types to template
+     *
+     * @return Race types list
+     */
+    @ModelAttribute("raceTypes")
+    public List<RaceType> raceTypes() {
+        return raceTypesRepository.findAllByOrderByIdAsc();
+    }
+
+    /**
+     * This method load all categories to template for build drop
+     * down select box
+     *
+     * @return All categories
+     */
+    @ModelAttribute("categories")
+    public List<Category> categories() {
+        return categoriesRepository.findAllByOrderByIdAsc();
     }
 
     /**
@@ -44,5 +91,18 @@ public class RacesController {
         ui.addAttribute("races", racesRepository.findAllByOrderByIdAsc(pageable));
 
         return "races";
+    }
+
+    /**
+     * Page for create new races
+     *
+     * @param ui Spring model ui
+     * @return Page for create new races
+     */
+    @GetMapping("/create")
+    public String create(final Model ui) {
+        ui.addAttribute("form", new Race());
+
+        return "race-create";
     }
 }
