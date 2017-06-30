f40ce639f4eff557017f932762e1466bd7dfb955
Krutikov <ka-spb@yandex.ru>
2017-06-30 13:16:26 +0300

    + aside: toggle open close

--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -6,7 +6,7 @@ btns.forEach((btn) => {
 	btn.onclick = (e) => {
 		e.preventDefault();
 		let attr = e.target.getAttribute('href'),
-				block = document.querySelector(attr);
+			block = document.querySelector(attr);
 
 		block.classList.toggle('active');
 
@@ -17,4 +17,12 @@ btns.forEach((btn) => {
 });
 
 document.getElementById('toggle_sidemenu_l')
-		.onclick = () => document.body.classList.toggle('active');
+	.onclick = () => {
+		if ( window.innerWidth > 1200 ) {
+			document.body.classList.toggle('toggle-close');
+			document.body.classList.remove('toggle-open');
+		} else {
+			document.body.classList.toggle('toggle-open');
+			document.body.classList.remove('toggle-close');
+		}
+};
