aff07ca644a289203b37a962de162cdcb890f562
Krutikov <ka-spb@yandex.ru>
2017-07-06 16:52:16 +0300

    + raceType: controller and page

--- src/main/java/pro/metrus/fvsr/controllers/TeamsController.java
+++ src/main/java/pro/metrus/fvsr/controllers/TeamsController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.metrus.fvsr.repositories.VidRepository;
+import pro.metrus.fvsr.repositories.RaceTypesRepository;
+
+import javax.websocket.server.PathParam;
 
 /**
  * This is teams controller.
@@ -14,17 +17,41 @@ import pro.metrus.fvsr.repositories.VidRepository;
 @RequestMapping("/race-types")
 public class TeamsController {
 
+    /**
+     * Repository for work with vid
+     */
     private final VidRepository vidRepository;
+    /**
+     * Repository for work with race types
+     */
+    private final RaceTypesRepository raceTypesRepository;
 
     /**
      * Constructor for dependency injection
      *
+     * @param raceTypesRepository Repository for work with "Типы заездов", must be not null
+     * @param vidRepository       Repository for work with "Виды", must be not null
      */
     @Autowired
     public TeamsController(
-            final VidRepository vidRepository
+            final VidRepository vidRepository,
+            final RaceTypesRepository raceTypesRepository
     ) {
         this.vidRepository = vidRepository;
+        this.raceTypesRepository = raceTypesRepository;
+    }
+
+    /**
+     * This method is simple. Method just return vid
+     * template name.
+     *
+     * @return Vids view name
+     */
+    @GetMapping
+    public String vid(final Model ui) {
+        ui.addAttribute("vid", vidRepository.findAllByOrderByIdAsc());
+
+        return "race-types";
     }
 
     /**
@@ -33,10 +60,10 @@ public class TeamsController {
      *
      * @return Teams view name
      */
-    @GetMapping
-    public String dictionaries(final Model ui) {
-        ui.addAttribute("vid", vidRepository.findAllByOrderByIdAsc());
+    @GetMapping("/{id}/members")
+    public String view(final Model ui, @PathParam("id") final long id) {
+        ui.addAttribute("raceTypes", raceTypesRepository.findAllByOrderByVidIdAsc());
 
-        return "race-types";
+        return "race-view";
     }
 }
